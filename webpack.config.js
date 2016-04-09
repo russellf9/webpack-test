d3a378ff53c7b0ccbd80025105992a230258c5c3
Russell Wenban <russell@factornine.co.uk>
2016-04-09 18:20:47 +0100

    – Add ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
+module.exports = {
 
-
-const merge = require('webpack-merge');
-
-const TARGET = process.env.npm_lifecycle_event;
-
-
-const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
-};
-
-const common = {
-    entry: PATHS.app,
+    entry: './app/app.ts',
     output: {
-        path: PATHS.build,
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
     },
     module: {
         loaders: [
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-
-                include: PATHS.app
-            }
+            { test: /\.ts$/,
+            loader: 'ts-loader'}
         ]
     }
-
 };
-
-// Default configuration
-if (TARGET === 'start' || !TARGET) {
-    module.exports = merge(common, {
-        devtool: 'eval-source-map',
-
-        devServer: {
-            contentBase: PATHS.build,
-
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            progress: true,
-
-
-            stats: 'errors-only',
-
-            host: process.env.HOST,
-            port: process.env.PORT
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new NpmInstallPlugin({
-                save: true // --save
-            })
-        ]
-    });
-}
-
-
-if (TARGET === 'build') {
-    module.exports = merge(common, {});
-}
-
