8dac1e263d88afa92ef84679539ca75991fc54f3
Russell Wenban <russell@factornine.co.uk>
2016-04-10 11:40:41 +0100

    – Add split configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,69 @@ const path = require('path');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
 
 
-module.exports = {
-    context: __dirname + '/app',
+const common = {
+    context: PATHS.app,
     entry: {
         index: './app'
     },
     output: {
-        path: __dirname + '/build',
+        path: PATHS.build,
         publicPath: '/',
         filename: 'bundle.js'
     },
     resolve: {
         root: __dirname,
-        extensions: ['', '.ts', '.js']
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
     },
     module: {
         loaders: [
             { test: /\.ts$/, loaders: ['ts-loader'], exclude: /node_modules/ }
         ]
-    },
-    devServer: {
-        contentBase: __dirname + '/build',
-
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-
-
-        stats: 'errors-only',
-
-        host: process.env.HOST,
-        port: process.env.PORT
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new NpmInstallPlugin({
-            save: true // --save
-        })
-    ]
+    }
 };
+
+// Note: `start` will be the package json cmd called
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+
+    module.exports = merge(common, {
+
+        devtool: 'eval-source-map',
+
+        devServer: {
+            contentBase: PATHS.build,
+
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            stats: 'errors-only',
+
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new NpmInstallPlugin({
+                save: true // --save
+            })
+        ]
+    });
+}
+
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
